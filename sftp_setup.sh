62bd65f3144c65647df93540600dae3113a58aa6
wazzanoski <warren_chisholm@hotmail.com>
2025-02-02 01:28:35 +1100

    Update sftp_setup.sh

--- sftp_setup.sh
+++ sftp_setup.sh
@@ -3,6 +3,7 @@
 APP_NAME=`basename -s '.sh' ${0}`
 BACKUPS_DIR="/backups"
 CONFIG_DIR="/config"
+USER_NAME="backtail"
 
 log() {
   MSG="${1}"
@@ -10,11 +11,6 @@ log() {
 }
 
 log "Begin."
-
-if [ -z "${SFTP_USER}" ] || [ -z "${SFTP_PASS}" ]; then
-  log "Error: SFTP_USER and SFTP_PASS must be set as environment variables."
-  exit 1
-fi
 
 if [ ! -f /config/ssh_host_rsa_key ]; then
   log "Generating SSH host keys..."
@@ -28,8 +24,8 @@ fi
 log "Setting up ftp user..."
 #Don't assign a password
 #Deny shell access
-adduser -D -h "${BACKUPS_DIR}" -s '/sbin/nologin' "${SFTP_USER}"
-echo "${SFTP_USER}:${SFTP_PASS}" | chpasswd
+adduser -D -h "${BACKUPS_DIR}" -s '/sbin/nologin' "${USER_NAME}"
+echo "${USER_NAME}:${USER_NAME}" | chpasswd
 
 log "Starting sshd..."
 /usr/sbin/sshd -D -e
