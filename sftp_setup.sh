9b83494597d070edf9af82c9ec843efc4ad11c5f
wazzanoski <warren_chisholm@hotmail.com>
2025-02-02 11:32:29 +1100

    Update sftp_setup.sh

--- sftp_setup.sh
+++ sftp_setup.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-APP_NAME=`basename -s '.sh' ${0}`
-USER_NAME="backtail"
+SCRIPT_NAME=`basename -s '.sh' ${0}`
 CONFIG_DIR="/config"
 
 log() {
-  printf "%s %-20s %s\n" "`date -Is`" "[${APP_NAME}]" "${*}"
+  printf "%s %-20s %s\n" "`date -Is`" "[${SCRIPT_NAME}]" "${*}"
 }
-
-log "Begin."
 
 if [ ! -f "${CONFIG_DIR}/ssh_host_ed25519_key" ]; then
   log "Generating SSH host key..."
@@ -16,18 +13,5 @@ if [ ! -f "${CONFIG_DIR}/ssh_host_ed25519_key" ]; then
 fi
 cp "${CONFIG_DIR}/ssh_host_ed25519_key" '/etc/ssh/'
 
-#log "Setting up sftp user..."
-#-S              Create a system user
-#-D              Don't assign a password
-#-H              Don't create home directory
-#adduser -S -D -H "${USER_NAME}"
-#Because the account was created without a password
-#the account is initially locked.
-#https://unix.stackexchange.com/questions/193066/how-to-unlock-account-for-public-key-ssh-authorization-but-not-for-password-aut
-#Unlock the account and set an invalid password hash:
-#echo "${USER_NAME}:*" | chpasswd
-
 log "Starting sshd..."
 /usr/sbin/sshd -D -e
-
-log "Done."
