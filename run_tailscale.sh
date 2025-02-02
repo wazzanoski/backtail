544355997a9fc9918bb3169c15d2711a991f7e83
wazzanoski <warren_chisholm@hotmail.com>
2025-02-02 13:31:45 +1100

    Update run_tailscale.sh

--- run_tailscale.sh
+++ run_tailscale.sh
@@ -6,17 +6,9 @@ TSD_ARG_STATEDIR="--statedir=${CONFIG_DIR}/.tailscaled_state"
 TSD_ARG_TUN="--tun=userspace-networking"
 TSD_LOG='/var/log/tailscaled'
 
-error_handler() {
-  echo
-  log "======================="
-  exit 1
-}
-
 log() {
   printf "%s %-20s %s\n" "`date -Is`" "[${SCRIPT_NAME}]" "${*}"
 }
-
-log "======================="
 
 log "Starting tailscaled..."
 log "tailscaled ${TSD_ARG_STATEDIR} ${TSD_ARG_TUN} >> ${TSD_LOG} 2>&1 &"
@@ -33,8 +25,9 @@ else
   log "ERROR: Connecting to Tailscale not successful!"
   if [ -f "${TSD_LOG}" ]; then
     log "Please check the logs:"
+    log "======================="
     tail -20 "${TSD_LOG}"
     log "======================="
   fi
-  error_handler
+  exit 1
 fi
