17a307e5b70c3e7988edc913b3bdcb3fcdba933b
wazzanoski <warren_chisholm@hotmail.com>
2025-02-02 13:25:59 +1100

    Update run_tailscale.sh

--- run_tailscale.sh
+++ run_tailscale.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-#Most of this script has been copied from here:
-#https://github.com/ich777/docker-steamcmd-server/blob/master/tailscale.sh
 
 SCRIPT_NAME=`basename -s '.sh' ${0}`
 CONFIG_DIR="/config"
-
-log() {
-  printf "%s %-20s %s\n" "`date -Is`" "[${SCRIPT_NAME}]" "${*}"
-}
+TSD_ARG_STATEDIR="--statedir=${CONFIG_DIR}/.tailscaled_state"
+TSD_ARG_TUN="--tun=userspace-networking"
+TSD_LOG='/var/log/tailscaled'
 
 error_handler() {
   echo
@@ -15,57 +12,28 @@ error_handler() {
   exit 1
 }
 
+log() {
+  printf "%s %-20s %s\n" "`date -Is`" "[${SCRIPT_NAME}]" "${*}"
+}
+
 log "======================="
 
-unset TSD_PARAMS
-unset TS_PARAMS
-
-if [ -z "${TAILSCALE_STAT_DIR}" ]; then
-  TAILSCALE_STATE_DIR="${CONFIG_DIR}/.tailscale_state"
-fi
-TSD_STATE_DIR=${TAILSCALE_STATE_DIR}
-log "Settings Tailscale state dir to: ${TSD_STATE_DIR}"
-
-if [ ! -d ${TS_STATE_DIR} ]; then
-  mkdir -p ${TS_STATE_DIR}
-fi
-
-TSD_PARAMS="${TSD_PARAMS}-tun=userspace-networking "  
-
-if [ "${TAILSCALE_LOG}" != "false" ]; then
-  TSD_PARAMS="${TSD_PARAMS}>>/var/log/tailscaled 2>&1 "
-  TSD_MSG=" with log file /var/log/tailscaled"
-else
-  TSD_PARAMS="${TSD_PARAMS}>/dev/null 2>&1 "
-fi
-
-if [ ! -z "${TAILSCALED_PARAMS}" ]; then
-  TSD_PARAMS="${TAILSCALED_PARAMS} ${TSD_PARAMS}"
-fi
-
-if [ ! -z "${TAILSCALE_PARAMS}" ]; then
-  TS_PARAMS="${TAILSCALE_PARAMS}${TS_PARAMS}"
-fi
-
 log "Starting tailscaled..."
-log "tailscaled -statedir=${TSD_STATE_DIR} ${TSD_PARAMS}&"
-eval tailscaled -statedir=${TSD_STATE_DIR} ${TSD_PARAMS}&
+log "tailscaled ${TSD_ARG_STATEDIR} ${TSD_ARG_TUN} >> ${TSD_LOG} 2>&1 &"
+eval tailscaled ${TSD_ARG_STATEDIR} ${TSD_ARG_TUN} >> ${TSD_LOG} 2>&1 &
 
 log "Starting tailscale..."
-log "tailscale up ${TS_AUTH}${TS_PARAMS}"
-eval tailscale up ${TS_AUTH}${TS_PARAMS}
+log "tailscale up"
+eval tailscale up
 EXIT_STATUS="$?"
 
 if [ "${EXIT_STATUS}" == "0" ]; then
   log "Connecting to Tailscale successful!"
-  if [ ! -f ${TSD_STATE_DIR}/.initialized ]; then
-    echo "Please don't remove this file!" > ${TSD_STATE_DIR}/.initialized
-  fi
 else
   log "ERROR: Connecting to Tailscale not successful!"
-  if [ -f /var/log/tailscaled ]; then
+  if [ -f "${TSD_LOG}" ]; then
     log "Please check the logs:"
-    tail -20 /var/log/tailscaled
+    tail -20 "${TSD_LOG}"
     log "======================="
   fi
   error_handler
