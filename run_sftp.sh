4e1518a7beef4f62017a09bb8959b50489985c0b
wazzanoski <warren_chisholm@hotmail.com>
2025-02-02 12:35:43 +1100

    Update and rename sftp_setup.sh to run_sftp.sh

--- run_sftp.sh
+++ run_sftp.sh
@@ -7,11 +7,14 @@ log() {
   printf "%s %-20s %s\n" "`date -Is`" "[${SCRIPT_NAME}]" "${*}"
 }
 
+#ssh host key
 if [ ! -f "${CONFIG_DIR}/ssh_host_ed25519_key" ]; then
   log "Generating SSH host key..."
   ssh-keygen -t ed25519 -f "${CONFIG_DIR}/ssh_host_ed25519_key" -N ""
 fi
 cp "${CONFIG_DIR}/ssh_host_ed25519_key" '/etc/ssh/'
+
+#banner
 if [ ! -f "${CONFIG_DIR}/banner.txt" ]; then
   log "Setting default banner..."
   cp '/etc/ssh/banner.txt' "${CONFIG_DIR}/" 
